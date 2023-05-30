1f4d6beb27185a1318f0eea5b0d73bdc1edf16fe
Afonso-Simoes <132764429+Afonso-Simoes@users.noreply.github.com>
2023-05-30 23:30:52 +0100

    Inicio
    Ainda não funciona

---
+++ src/AppA.js
@@ -0,0 +1,19 @@
+import "./assets/styles/App.css";
+import APainelJogo from "./components/APainelJogo/APainelJogo.component";
+
+function App() {
+  return (
+    <div id="container">
+      <h2>Ultimate TIC TAC TOE em React</h2>
+      <h3>Linguagens Script</h3>
+      <GamePanel>
+        APainelJogo={}
+      </GamePanel>
+    </div>
+  );
+}
+
+export default App;
+// Esta linha também poderia ser eliminada
+// e adefinição da funsão ser substituida 
+// export default function App() {
---
+++ src/components/APainelJogo/APainelJogo.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./APainelJogo.css";
+
+function APainelJogo(){
+    return (
+        <section className="PainelJogo">
+            <div id="PainelJogo">
+                
+            </div>
+        </section>
+    );
+}
+
+export default APainelJogo;

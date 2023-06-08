b3180da450011a36ce5483a624884fa569a16ce4
Afonso-Simoes <132764429+Afonso-Simoes@users.noreply.github.com>
2023-06-09 00:20:21 +0100

    Ja Funciona (Chatgpt)

--- src/components/APainelJogo/APainelJogo.component.jsx
+++ src/components/APainelJogo/APainelJogo.component.jsx
@@ -1,279 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./APainelJogo.css";
 // import NoveTabuleirosJogo from "../components/A9TabuleirosJogo/A9TabuleirosJogo.component";
 // import NoveTabuleirosJogo from "src\components\A9TabuleirosJogo\A9TabuleirosJogo.jsx";
 
-function PainelJogo({player}){
+function PainelJogo({ player }) {
+    const [buttonValues, setButtonValues] = useState(Array(81).fill(null));
+    const [currentPlayer, setCurrentPlayer] = useState("X");
+  
+    const handleButtonClick = (index) => {
+      const newButtonValues = [...buttonValues];
+      newButtonValues[index] = currentPlayer;
+      setButtonValues(newButtonValues);
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+    };
+  
     return (
-        <section id="PainelJogo">
-            <div id="NoveTabuleirosJogo">
-                <div id="TabuleiroJogo1" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo2" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo3" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo4" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo5" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo6" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo7" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo8" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
-                <div id="TabuleiroJogo9" className="Tabuleiros">
-                    <button class="Quadrado1" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado2" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado3" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado4" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado5" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado6" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado7" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado8" className="Quadrados">
-
-                    </button>
-                    <button class="Quadrado9" className="Quadrados">
-
-                    </button>
-                </div>
+      <section id="PainelJogo">
+        <div id="NoveTabuleirosJogo">
+          {[...Array(9)].map((_, tabIndex) => (
+            <div key={`TabuleiroJogo${tabIndex + 1}`} className="Tabuleiros">
+              {[...Array(9)].map((_, buttonIndex) => {
+                const index = tabIndex * 9 + buttonIndex;
+                return (
+                  <button
+                    key={`Quadrado${index + 1}`}
+                    className="Quadrados"
+                    onClick={() => handleButtonClick(index)}
+                  >
+                    {buttonValues[index]}
+                  </button>
+                );
+              })}
             </div>
-        </section>
-    //     for(let i = 0; i < 9; i++){
-            
-    //     }
+          ))}
+        </div>
+      </section>
     );
-}
+  }
 
 export default PainelJogo;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import "./assets/styles/w3.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppR />
+    <AppA />
   </React.StrictMode>
 );

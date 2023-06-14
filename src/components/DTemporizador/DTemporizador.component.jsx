bb8c982a97921e027bf3a6fe86bcd6e54b14f692
Afonso-Simoes <132764429+Afonso-Simoes@users.noreply.github.com>
2023-06-14 17:05:28 +0100

    Temporizadores sem mudarem o tempo

--- src/components/DTemporizador/DTemporizador.component.jsx
+++ src/components/DTemporizador/DTemporizador.component.jsx
@@ -1,42 +1,91 @@
 import React, { useEffect, useState } from "react";
 import "./DTemporizador.css";
 
-function Temporizador() {
-  const [tempoRestante, setTempoRestante] = useState(120);
-  const [contadorAtivo, setContadorAtivo] = useState(false);
+function Temporizador(props) {
+    const [tempoRestante_X, setTempoRestante_X] = useState(120);
+    const [tempoRestante_O, setTempoRestante_O] = useState(120);
+    const [contadorAtivo, setContadorAtivo] = useState(true);
+    const [tempoInicial_X, setTempoInicial_X] = useState(120);
+    const [tempoInicial_O, setTempoInicial_O] = useState(120);
 
-  useEffect(() => {
-    let intervalId;
+    useEffect(() => {
+        let intervalId;
+    
+        if (contadorAtivo && !props.pausarContador_X) {
+          intervalId = setInterval(() => {
+                setTempoRestante_X((prevTempo) => {
+                    if (prevTempo > 0) {
+                        return prevTempo - 1;
+                    } else {
+                        setContadorAtivo(false);
+                        return 0;
+                    }
+                });
+          }, 1000);
+        }
+    
+        return () => clearInterval(intervalId);
+    }, [contadorAtivo, props.pausarContador_X]);
 
-    if (contadorAtivo) {
-      intervalId = setInterval(() => {
-        setTempoRestante((prevTempo) => prevTempo - 1);
-      }, 1000);
-    }
 
-    return () => clearInterval(intervalId);
-  }, [contadorAtivo]);
+    useEffect(() => {
+        let intervalId;
+    
+        if (contadorAtivo && !props.pausarContador_O) {
+            intervalId = setInterval(() => {
+                setTempoRestante_O((prevTempo) => {
+                    if (prevTempo > 0) {
+                        return prevTempo - 1;
+                    } else {
+                        setContadorAtivo(false);
+                        return 0;
+                    }
+                });
+            }, 1000);
+        }
+    
+        return () => clearInterval(intervalId);
+    }, [contadorAtivo, props.pausarContador_O]);
 
-  useEffect(() => {
-    const delay = setTimeout(() => {
-      setContadorAtivo(true);
-    }, 2000);
+    useEffect(() => {
+        if (props.currentPlayer === "X") {
+            setContadorAtivo(true);
+            setTempoInicial_X(tempoRestante_X);
+        } else {
+            setContadorAtivo(false);
+        }
+    }, [props.currentPlayer, tempoRestante_X]);
 
-    return () => clearTimeout(delay);
-  }, []);
+    useEffect(() => {
+        if (props.currentPlayer === "O") {
+            setContadorAtivo(true);
+            setTempoInicial_O(tempoRestante_O);
+        } else {
+            setContadorAtivo(false);
+        }
+    }, [props.currentPlayer, tempoRestante_O]);
 
-  const formatarTempo = (tempo) => {
-    const minutos = Math.floor(tempo / 60);
-    const segundos = tempo % 60;
+    const formatarTempo = (tempo) => {
+        const minutos = Math.floor(tempo / 60);
+        const segundos = tempo % 60;
 
-    return `${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
-  };
+        return `${minutos.toString().padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
+    };
 
-  return (
-    <div className={`temporizador-container ${contadorAtivo ? "ativo" : ""}`}>
-      <div className="temporizador-display">{formatarTempo(tempoRestante)}</div>
-    </div>
-  );
+    return (
+        // <div className={`temporizador-container ${contadorAtivo ? "ativo" : ""}`}>
+        //   <div className="temporizador-display">{formatarTempo(tempoRestante)}</div>
+        //   {!contadorAtivo && <button onClick={() => setContadorAtivo(true)}>Iniciar</button>}
+        //   {contadorAtivo && <button onClick={pausarTemporizador}>Pausar</button>}
+        // </div>
+        <div className={`temporizador-container ${props.currentPlayer}`}>
+            {props.currentPlayer === "X" ? <div className="temporizador-display">{formatarTempo(tempoRestante_X)}</div> : <div className="temporizador-display">{formatarTempo(tempoRestante_O)}</div>}
+            {/* <div className="temporizador-display">{formatarTempo(tempoRestante_X)}</div>
+            {(contadorAtivo === true ) ? 
+                <div className="temporizador-display">{formatarTempo(tempoRestante_X)}</div> : <div className="temporizador-display">{formatarTempo(tempoRestante_X)}</div>
+            } */}
+        </div>
+    );
 }
 
 export default Temporizador;
